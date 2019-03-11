601fcabe18be51e40b653ce878aea4752daae291
FraLotito <lotitoqf@gmail.com>
2019-03-11 16:25:47 +0100

    upload

---
+++ frog1.cc
@@ -0,0 +1,37 @@
+//https://atcoder.jp/contests/dp/tasks/dp_a
+
+#include<bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+using namespace std;
+
+using ll = long long;
+using ii = pair<int, int>;
+const int INF = (int)10e9;
+int N;
+vector<int> v;
+vector<int> dp;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	
+	cin >> N;
+	v.resize(N);
+	dp.resize(N);
+	fill(all(dp), 0);
+	
+	for(int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+	
+	for(int i = 1; i < N; i++) {
+		if(i == 1) dp[i] = abs(v[i] - v[i-1]) + dp[i-1];
+		else dp[i] = min(abs(v[i] - v[i-1]) + dp[i-1], abs(v[i] - v[i-2]) + dp[i-2]);
+	}
+	
+	cout << dp[N-1] << endl;
+
+    return 0;
+}
---
+++ frog2.cc
@@ -0,0 +1,39 @@
+//https://atcoder.jp/contests/dp/tasks/dp_b
+
+#include<bits/stdc++.h>
+#define all(v) v.begin(), v.end()
+#define mp make_pair
+using namespace std;
+
+using ll = long long;
+using ii = pair<int, int>;
+const int INF = (int)10e9;
+int N, K;
+vector<int> v;
+vector<int> dp;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	
+	cin >> N >> K;
+	v.resize(N);
+	dp.resize(N);
+	fill(all(dp), 0);
+	
+	for(int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+	
+	for(int i = 1; i < N; i++) {
+		dp[i] = INF;
+		for(int j = 1; j <= K; j++) {
+			if(i - j >= 0) dp[i] = min(dp[i], abs(v[i] - v[i-j]) + dp[i-j]);
+		}
+	}
+	
+	cout << dp[N-1] << endl;
+
+    return 0;
+}
