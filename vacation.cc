de3689dfa589cec7e7ab2a905c809f581cd9b880
FraLotito <lotitoqf@gmail.com>
2019-03-12 11:21:53 +0100

    fix indentation

--- vacation.cc
+++ vacation.cc
@@ -11,26 +11,26 @@ const int INF = (int)10e9;
 int dp[3][100005];
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	int N;
-	cin>>N;
-	vector<int> a(N), b(N), c(N);
-	for(int i = 0; i < N; i++) {
-		cin>>a[i]>>b[i]>>c[i];
-	}
-	
-	dp[0][0] = a[0];
-	dp[1][0] = b[0];
-	dp[2][0] = c[0];
-	for(int i = 1; i < N; i++) {
-		dp[0][i] = a[i] + max(dp[1][i-1], dp[2][i-1]);
-		dp[1][i] = b[i] + max(dp[0][i-1], dp[2][i-1]);
-		dp[2][i] = c[i] + max(dp[0][i-1], dp[1][i-1]);
-	}
-	cout << max(max(dp[0][N-1], dp[1][N-1]), dp[2][N-1]) <<endl;
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin>>N;
+    vector<int> a(N), b(N), c(N);
+    for(int i = 0; i < N; i++) {
+        cin>>a[i]>>b[i]>>c[i];
+    }
+
+    dp[0][0] = a[0];
+    dp[1][0] = b[0];
+    dp[2][0] = c[0];
+    for(int i = 1; i < N; i++) {
+        dp[0][i] = a[i] + max(dp[1][i-1], dp[2][i-1]);
+        dp[1][i] = b[i] + max(dp[0][i-1], dp[2][i-1]);
+        dp[2][i] = c[i] + max(dp[0][i-1], dp[1][i-1]);
+    }
+    cout << max(max(dp[0][N-1], dp[1][N-1]), dp[2][N-1]) <<endl;
 
     return 0;
 }
