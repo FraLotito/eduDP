4a89e8ee84927b0d467b8b39773b9bf473e15714
FraLotito <lotitoqf@gmail.com>
2019-03-12 11:21:26 +0100

    fix indentation

--- frog2.cc
+++ frog2.cc
@@ -16,24 +16,24 @@ int main() {
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-	
-	cin >> N >> K;
-	v.resize(N);
-	dp.resize(N);
-	fill(all(dp), 0);
-	
-	for(int i = 0; i < N; i++) {
-		cin >> v[i];
-	}
-	
-	for(int i = 1; i < N; i++) {
-		dp[i] = INF;
-		for(int j = 1; j <= K; j++) {
-			if(i - j >= 0) dp[i] = min(dp[i], abs(v[i] - v[i-j]) + dp[i-j]);
-		}
-	}
-	
-	cout << dp[N-1] << endl;
+
+    cin >> N >> K;
+    v.resize(N);
+    dp.resize(N);
+    fill(all(dp), 0);
+
+    for(int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    for(int i = 1; i < N; i++) {
+        dp[i] = INF;
+        for(int j = 1; j <= K; j++) {
+            if(i - j >= 0) dp[i] = min(dp[i], abs(v[i] - v[i-j]) + dp[i-j]);
+        }
+    }
+
+    cout << dp[N-1] << endl;
 
     return 0;
 }
