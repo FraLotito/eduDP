1df895f9a9652ba2449ef650a04cc05c8009c992
FraLotito <lotitoqf@gmail.com>
2019-03-12 17:34:01 +0100

    fix

--- longest_path.cc
+++ longest_path.cc
@@ -16,39 +16,39 @@ stack<int> ts;
 bool vis[100005];
 
 void dfs(int n) {
-	if(!vis[n]) {
-		vis[n] = true;
-		for(int i = 0; i < g[n].size(); i++) dfs(g[n][i]);
-		ts.push(n);
-	}
+    if(!vis[n]) {
+        vis[n] = true;
+        for(int i = 0; i < g[n].size(); i++) dfs(g[n][i]);
+        ts.push(n);
+    }
 }
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	
-	cin >> N >> M;
-	
-	for(int i = 0; i < M; i++){
-		int a, b;
-		cin >> a >> b;
-	    g[a].push_back(b);
-	}
-	
-	for(int i = 1; i <= N; i++) {
-		if(!vis[i]) dfs(i);
-	}
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	while(!ts.empty()) {
-		int n = ts.top();
-		for(int i = 0;i < g[n].size(); i++) dp[g[n][i]] = max(dp[g[n][i]], dp[n] + 1);
-		ts.pop();
-	}
+    cin >> N >> M;
 
-	int res = 0;
-	for(int i = 0; i <= N; i++) res = max(res, dp[i]);
-	cout<<res<<endl;
+    for(int i = 0; i < M; i++){
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+    }
+
+    for(int i = 1; i <= N; i++) {
+        if(!vis[i]) dfs(i);
+    }
+
+    while(!ts.empty()) {
+        int n = ts.top();
+        for(int i = 0;i < g[n].size(); i++) dp[g[n][i]] = max(dp[g[n][i]], dp[n] + 1);
+        ts.pop();
+    }
+
+    int res = 0;
+    for(int i = 0; i <= N; i++) res = max(res, dp[i]);
+    cout<<res<<endl;
 
     return 0;
 }
