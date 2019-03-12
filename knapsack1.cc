7a2def4f6fc16593603d7003e3c5ad14e7850fb1
FraLotito <lotitoqf@gmail.com>
2019-03-12 11:21:41 +0100

    fix indentation

--- knapsack1.cc
+++ knapsack1.cc
@@ -10,24 +10,24 @@ int N, W;
 ll dp[105][100005];
 
 int main() {
-	std::ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> N >> W;
-	vector<int> w(N), v(N);
-	for(int i = 0; i < N; i++) {
-		cin>>w[i]>>v[i];
-	}
-	
-	for(int i = 0; i < N; i++) {
-		for(int j = 0; j <= W; j++) {
-			if(i == 0) { if(j >= w[i]) dp[i][j] = v[i];}
-			else if(j - w[i] >= 0) dp[i][j] = max(v[i] + dp[i-1][j-w[i]], dp[i-1][j]);
-			else dp[i][j] = dp[i-1][j];
-		}
-	}
-	
-	cout<<dp[N-1][W]<<endl;
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> W;
+    vector<int> w(N), v(N);
+    for(int i = 0; i < N; i++) {
+        cin>>w[i]>>v[i];
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j <= W; j++) {
+            if(i == 0) { if(j >= w[i]) dp[i][j] = v[i];}
+            else if(j - w[i] >= 0) dp[i][j] = max(v[i] + dp[i-1][j-w[i]], dp[i-1][j]);
+            else dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    cout<<dp[N-1][W]<<endl;
 
     return 0;
 }
